3e16f7abb5749a32355a3a870a7a31701ca53afe
zeroninefourfour <zwec181818@proton.me>
2026-02-27 16:09:12 +0800

    addded keypress listening

--- scenes/gameplay.lua
+++ scenes/gameplay.lua
@@ -3,6 +3,7 @@ local gameplay = {}
 local note_icon = love.graphics.newImage("assets/note.png")
 local scroll_speed = 64.0
 
+local debug_note = false
 -- Playback things
 local audioInstance = nil
 local playback = {
@@ -12,16 +13,15 @@ local playback = {
 }
 
 local near_notes = {}
-local notes = {{
-    type = "cricital",
-    y = 0.5,
-    time = 0.1
-}, {
-    type = "cricital",
-    y = 0.5,
-    time = 10
-}}
+local notes = {}
 
+for i = 1, 100 do
+    table.insert(notes, {
+        type = "cricital",
+        y = 0.5,
+        time = 15 + i * 5
+    })
+end
 local last_visible_note_index = 1
 -- Playback things
 
@@ -42,50 +42,68 @@ gameplay.update = function()
 end
 
 gameplay.drawNote = function(note_HitTime, Y_Position)
-    local hitX = 100    
-love.graphics.draw(
-    note_icon,
-    hitX + (note_HitTime - playback.time) * scroll_speed,
-    Y_Position,
-    0, 0.05, 0.05
-)
+    local hitX = 100
+    love.graphics.draw(note_icon, hitX + (note_HitTime - playback.time) * scroll_speed, Y_Position, 0, 0.05, 0.05)
 end
-
+gameplay.drawJudgementText = function(type)
+    love.graphics.print(type, 200, 200)
+end
 gameplay.drawJudgementLine = function()
-
     love.graphics.setColor(1, 1, 1, 0.5)
     love.graphics.setLineWidth(2)
     love.graphics.line(100, love.graphics.getHeight(), 100, 0);
 end
 gameplay.draw = function()
     love.graphics.setColor(1, 1, 1)
- 
-    gameplay.drawJudgementLine()
-    print(playback.time)
-                 near_notes = {}
 
-    for index, note in ipairs(notes) do
-        local calculated_note_appearX = 100 + (note.time * scroll_speed)
+    gameplay.drawJudgementLine()
+    -- print(playback.time)
+    near_notes = {}
+
+    for index, note in pairs(notes) do
+        local calculated_note_appearX = note.time + (note.time - playback.time) * scroll_speed
         -- Check if the note is within the visible area
-        print("Note Calculation: ", note.time, playback.time, calculated_note_appearX)
+       if debug_note then
+ print("Note Calculation: ", note.time, playback.time, calculated_note_appearX)
         print("Note Abs: ", math.abs(note.time - playback.time))
         print("NearNotes: ", #near_notes)
+        print("NoteCounts: ", #notes)        end
         if (math.abs(note.time - playback.time) < 1) then
-              table.insert(near_notes, note)
+            table.insert(near_notes, note)
         end
         if (calculated_note_appearX > love.graphics.getWidth()) then
             break -- No need to draw notes that are off-screen to the right
         end
-         
+
         gameplay.drawNote(note.time, note.y * love.graphics.getHeight())
     end
-    
-     
+
 end
 
 function gameplay.keypressed(key, scancode, isrepeat)
-   if key == "z" or key == "x" then
-      print("Hit")
-   end
+    print(key  )
+    if key == "z" or key == "x" then
+       for index, note in pairs(near_notes) do
+            if math.abs(note.time - playback.time) < 0.5 then
+                local latency = math.abs(note.time - playback.time)
+                print("Hit! Latency: ", latency)
+                if latency < 0.05 then
+                    print("Cricital Perfect!")
+                elseif latency < 0.1 then
+                    print("Perfect!")
+                elseif latency < 0.25 then
+                    print("Great!")
+                elseif latency < 1 then
+                    print("Good!")
+                elseif latency < 1.5 then
+                    print("Bad!")
+                else
+                    print("Miss!")
+                end
+                 break
+            end
+
+        end
+    end
 end
 return gameplay;
